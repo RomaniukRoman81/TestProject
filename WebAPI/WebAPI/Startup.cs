391e7394a53499824ac9bc878f5cf741d9f8a2b8
Roman Romanyuk <roman.romanyuk@quantumit.com.au>
2020-01-23 21:49:43 +0200

    A7 test commit

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -14,7 +14,7 @@ namespace WebAPI
         {
             Configuration = configuration;
         }
-
+        // for test commit
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
