1159bc3d57fce9e12ca6f443b4706ad41b32c25c
Roman Romanyuk <roman.romanyuk@quantumit.com.au>
2020-03-24 20:43:47 +0200

    TP-2403 added first unit test

---
+++ WebAPI/TestProject.Test/PaymentDetail/PaymentDetailServiceTestBase.cs
@@ -0,0 +1,38 @@
+﻿using TestProject.Data.Models;
+using TestProject.Services;
+
+namespace TestProject.Test.PaymentDetail
+{
+    public class PaymentDetailServiceTestBase
+    {
+        private readonly AuthenticationContext _context;
+        public PaymentDetailServiceTestBase(AuthenticationContext context)
+        {
+            _context = context;
+        }
+
+        protected void CreateTestData()
+        {
+            var paymentDetail1 = new TestProject.Data.Models.PaymentDetail
+            {
+                Id = 1,
+                CardOwnerName = "TestCardOwner1",
+                CardNumber = "1111111111111111",
+                ExpirationDate = "02/25",
+                CVV = "111"
+            };
+
+            var paymentDetail2 = new TestProject.Data.Models.PaymentDetail
+            {
+                Id = 2,
+                CardOwnerName = "TestCardOwner2",
+                CardNumber = "1111111111111112",
+                ExpirationDate = "02/24",
+                CVV = "112"
+            };
+
+            _context.AddRange(paymentDetail1, paymentDetail2);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ WebAPI/TestProject.Test/PaymentDetail/PaymentDetailService_Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestProject.Data.Models;
+using TestProject.Services;
+using Xunit;
+
+namespace TestProject.Test.PaymentDetail
+{
+    public class PaymentDetailService_Test : PaymentDetailServiceTestBase
+    {
+        private readonly IPaymentDetailService _paymentDetailService;
+
+        public PaymentDetailService_Test(IPaymentDetailService paymentDetailService, AuthenticationContext context) : base(context)
+        {
+            _paymentDetailService = paymentDetailService;
+            CreateTestData();
+        }
+
+        [Fact]
+        public async Task Should_Get_All_PaymentDetails_Return_List()
+        {
+            var paymentDetails = await _paymentDetailService.GetAllAsync();
+
+            //Assert  
+            Assert.IsType<OkObjectResult>(paymentDetails);
+        }
+
+    }
+}
