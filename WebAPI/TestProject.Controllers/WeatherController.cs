bfb816758bce7b4eebe207c7e0828d3f6f289351
roman romaniuk <roman.romanyuk@quantumit.com.au>
2020-05-01 17:31:31 +0300

    TP-0105/1 added config & dto

--- WebAPI/TestProject.Controllers/WeatherController.cs
+++ WebAPI/TestProject.Controllers/WeatherController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using TestProject.Controllers.Models;
+using TestProject.Services.Models;
 
 namespace TestProject.Controllers
 {
@@ -14,6 +16,12 @@ namespace TestProject.Controllers
     [ApiController]
     public class WeatherController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
+        public WeatherController(IConfiguration config)
+        {
+            _config = config;
+        }
 
         [HttpGet("{city}")]
         public async Task<IActionResult> City(string city)
@@ -22,15 +30,27 @@ namespace TestProject.Controllers
             {
                 try
                 {
-                    var test = "49db1eddb87f3de01a97c70a36db95b9";
-                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var test = _config.GetValue<string>("OpenWeather:MyAPIKey");
+                    var url = _config.GetValue<string>("OpenWeather:BaseAddress");
+                    client.BaseAddress = new Uri(url);
                     var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid={test}&units=metric");
                     response.EnsureSuccessStatusCode();
 
                     var stringResult = await response.Content.ReadAsStringAsync();
                     var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
 
-                    return Ok(rawWeather);
+                    var result = new OpenWeatherResponseDto
+                    {
+                        CityName = rawWeather.Name,
+                        WeatherMain = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        WeatherDescription = string.Join(",", rawWeather.Weather.Select(x => x.Description)),
+                        WeatherIcon = rawWeather.Weather.First().Icon,
+                        Temp = rawWeather.Main.Temp,
+                        TempFeelsLike = rawWeather.Main.Feels_Like,
+                        WindSpeed = rawWeather.Wind.Speed
+                    };
+
+                    return Ok(result);
                 }
                 catch ( HttpRequestException httpRequestException )
                 {
---
+++ WebAPI/TestProject.Services/Models/OpenWeatherResponseDto.cs
@@ -0,0 +1,19 @@
+﻿namespace TestProject.Services.Models
+{
+    public class OpenWeatherResponseDto
+    {
+        public string CityName { get; set; }
+
+        public string WeatherMain { get; set; }
+
+        public string WeatherDescription { get; set; }
+
+        public string WeatherIcon { get; set; }
+
+        public string Temp { get; set; }
+
+        public string TempFeelsLike { get; set; }
+
+        public string WindSpeed { get; set; }
+    }
+}
