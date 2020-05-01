39af5d12107c005c3c0277748fdb09a5328cd0dd
roman romaniuk <roman.romanyuk@quantumit.com.au>
2020-05-01 16:27:55 +0300

    TP-0105 added back-end part for getting weather data

---
+++ WebAPI/TestProject.Controllers/Models/OpenWeatherResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace TestProject.Controllers.Models
+{
+    public class OpenWeatherResponse
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<WeatherDescription> Weather { get; set; }
+
+        public Main Main { get; set; }
+
+        public Wind Wind { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Main { get; set; }
+
+        public string Description { get; set; }
+
+        public string Icon { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+
+        public string Feels_Like { get; set; }
+    }
+
+    public class Wind
+    {
+        public string Speed { get; set; }
+    }
+}
---
+++ WebAPI/TestProject.Controllers/WeatherController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.Controllers.Models;
+
+namespace TestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+
+        [HttpGet("{city}")]
+        public async Task<IActionResult> City(string city)
+        {
+            using ( var client = new HttpClient() )
+            {
+                try
+                {
+                    var test = "49db1eddb87f3de01a97c70a36db95b9";
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid={test}&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+
+                    return Ok(rawWeather);
+                }
+                catch ( HttpRequestException httpRequestException )
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+    }
+}
